2085b2da56c0ed6a675d6a3a3bb6842e453f3071
Dahoon Song <dhsong@berkeley.edu>
2020-05-26 23:53:22 +0900

    add firebase api key to protected .env file

--- react-chat/src/firebase.js
+++ react-chat/src/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase';
 import 'firebase/auth';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyB_cw_8Km5-cwotQjFphuG0At5CqSB2tZ0",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "react-chat-5a4ec.firebaseapp.com",
   databaseURL: "https://react-chat-5a4ec.firebaseio.com",
   projectId: "react-chat-5a4ec",
