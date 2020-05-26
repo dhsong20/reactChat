bd2c0629166880da00a36434f18427845b0b6a68
Dahoon Song <dhsong@berkeley.edu>
2020-05-26 23:31:39 +0900

    login finished, personalized message todo

--- react-chat/src/components/login/Login.js
+++ react-chat/src/components/login/Login.js
@@ -1,6 +1,6 @@
 import React, { useCallback, useContext } from 'react';
 import { Link, withRouter, Redirect } from 'react-router-dom';
-import AuthContext from '../../Auth'
+import { AuthContext } from '../../Auth'
 import firebase from '../../firebase';
 
 function Login({ history }) {
@@ -27,6 +27,7 @@ function Login({ history }) {
       
       <div className="authenticationWrapper">
         <h1>Hello!</h1>
+        <h2>Log in for a personalized message :)</h2>
         <form onSubmit={handleLogin}>
           <label>
             Email
